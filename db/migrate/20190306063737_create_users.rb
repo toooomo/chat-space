95c0cf3e53ca734822b0ac12fd1e98e1115a792b
toooomo <tomobase01220@me.com>
2019-03-06 20:26:10 +0900

    Change

--- db/migrate/20190306063737_create_users.rb
+++ db/migrate/20190306063737_create_users.rb
@@ -3,8 +3,6 @@ class CreateUsers < ActiveRecord::Migration[5.0]
     create_table :users do |t|
       t.string     :name, null: false
       t.string     :email, unique: true
-      t.integer    :group_id
-      t.text       :messages, foreign_key: true
       t.timestamps
     end
   end
--- db/migrate/20190306064006_create_groups.rb
+++ db/migrate/20190306064006_create_groups.rb
@@ -1,9 +1,7 @@
 class CreateGroups < ActiveRecord::Migration[5.0]
   def change
     create_table :groups do |t|
-      t.string     :name
-      t.integer    :user_id, null: false, foreign_key: true
-      t.text       :messages, foreign_key: true
+      t.string      :name
       t.timestamps
     end
   end
