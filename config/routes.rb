0d741e989c0c2340dbdf70719b2db17272a6d510
toooomo <tomobase01220@me.com>
2019-03-09 17:42:35 +0900

    Group-view-controller-model

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'messages#index'
-  resources :users, only: [:edit, :update]
-  resources :messages
+  resources :users, only: [:index, :edit, :update]
+
+  resources :groups, only: [:new, :create, :edit, :update]
+    resources :messages, only: [:index, :create]
 end
