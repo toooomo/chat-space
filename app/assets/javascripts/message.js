180159b1a1cc60a5ec40b6bd71e58c6fef08184c
toooomo <tomobase01220@me.com>
2019-03-20 16:50:51 +0900

    change_name

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,5 +1,5 @@
 $(function(){
-  function buildHTML(message){
+  function buildMessageHTML(message){
     if (message.image) {
       var html = `<div class="message">
                     <div class="message__info">
@@ -49,8 +49,8 @@ $(function(){
       processData: false,
       contentType: false
     })
-     .done(function(data){
-       var html = buildHTML(data);
+     .done(function(newMessage){
+       var html = buildMassegeHTML(newMessage);
        $('.messages').append(html);
        $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
        $('form')[0].reset();
