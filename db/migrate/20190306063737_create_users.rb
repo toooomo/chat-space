6aa9f16fca271b5ce5408756a7809c951492cd80
toooomo <tomobase01220@me.com>
2019-03-30 17:16:22 +0900

    Delete-migrate

--- db/migrate/20190306063737_create_users.rb
+++
@@ -1,9 +0,0 @@
-class CreateUsers < ActiveRecord::Migration[5.0]
-  def change
-    create_table :users do |t|
-      t.string     :name, null: false
-      t.string     :email, unique: true
-      t.timestamps
-    end
-  end
-end
